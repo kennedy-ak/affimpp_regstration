edc7f31e1aad9b944e77e2f2d7d09bb800b3d0e0
kennedy-ak <akogokennedy@gmail.com>
2024-12-30 18:07:48 +0000

    allowed host

--- affimpp_regstration/settings.py
+++ affimpp_regstration/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@y*6bkj#w@6&$lzy!s=_v&f7=)7@6sff@mq4i#v%8l&7p+3*&@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
