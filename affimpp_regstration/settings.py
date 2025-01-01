607f6f465f5c58a5b26ed24b292f0d21a2db1358
kennedy-ak <akogokennedy@gmail.com>
2025-01-01 20:52:10 +0000

    add

--- affimpp_regstration/settings.py
+++ affimpp_regstration/settings.py
@@ -23,13 +23,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 # .env
 
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY='django-insecure-@y*6bkj#w@6&$lzy!s=_v&f7=)7@6sff@mq4i#v%8l&7p+3*&@'
 
 
-DEBUG = os.environ.get('DEBUG',"False").lower == "true"
+DEBUG=True
+
+ALLOWED_HOSTS= ["*"]
 
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(" ")
 
 # Application definition
 
